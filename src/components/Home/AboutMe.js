7ce0b222fc054e51ad3d6a31b567c5b17f241eb6
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-06-27 11:23:24 +0600

    favicon add

--- src/components/Home/AboutMe.js
+++ src/components/Home/AboutMe.js
@@ -10,27 +10,29 @@ const AboutMe = () => {
                     <Fade left
 
                     >
-                        
-                            <img
-                                src={about} class="xl:w-[320px] img lg:w-[240px] w-[180px] object-cover rounded-lg " alt='aboutImg' />
-                       
+
+                        <img
+                            src={about} class="xl:w-[320px] img lg:w-[240px] w-[180px] object-cover rounded-lg " alt='aboutImg' />
+
                     </Fade>
-                    <Fade right>
-                        <div
-                            data-aos="zoom-in"
-                            data-aos-duration="1000"
-                            className='max-w-lg '>
-                            <h1 class="xl:text-5xl lg:text-4xl text-3xl font-bold">I am <span className='text-[#037fff]'>Ibrahim Kholil</span></h1>
-                            <p class="py-6 lg:text-[16px] xl:text-[18px] text-[16px]">Hi There,I am Junior full stack developer.Specially I am a MERN Stack Developer.I can provide clean code and pixel perfect website.I will make your website more interactive and more featurette</p>
-                            <div>
+                    <div >
+                        <Fade right>
+                            <div
+                                data-aos="zoom-in"
+                                data-aos-duration="1000"
+                                className='max-w-lg mt-[70px]'>
+                                <h1 class="xl:text-5xl lg:text-4xl text-3xl font-bold">I am <span className='text-[#037fff]'>Ibrahim Kholil</span></h1>
+                                <p class="py-6 lg:text-[16px] xl:text-[18px] text-[16px]">Hi There,I am Junior full stack developer.Specially I am a MERN Stack Developer.I can provide clean code and pixel perfect website.I will make your website more interactive and more featurette</p>
                                 <div>
-                                    <h1 className='text-xl'>Name : Ibrahim Kholil</h1>
-                                    <h1 className='text-xl'>Email : kholilmdibrahim17@gmail.com</h1>
-                                    <h1 className='text-xl'>Phone : 01741313873 </h1>
+                                    <div>
+                                        <h1 className='text-xl'>Name : Ibrahim Kholil</h1>
+                                        <h1 className='text-xl'>Email : kholilmdibrahim17@gmail.com</h1>
+                                        <h1 className='text-xl'>Phone : 01741313873 </h1>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </Fade>
+                        </Fade>
+                    </div>
                 </div>
             </div>
         </div>
